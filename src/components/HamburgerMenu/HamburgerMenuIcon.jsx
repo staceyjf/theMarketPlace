a9b5e31dc1997b740453aa49e11f4344dff01d6b
staceyjf <staceyfanner@gmail.com>
2024-04-21 23:17:05 +1000

    cartPage render logic

--- src/components/HamburgerMenu/HamburgerMenuIcon.jsx
+++ src/components/HamburgerMenu/HamburgerMenuIcon.jsx
@@ -1,7 +1,6 @@
 import styles from "./HamburgerMenuIcon.module.scss";
 
 function HamburgerMenuIcon({ isOpen }) {
-  console.log(isOpen);
   return (
     <div className={styles.hamburger_menu}>
       <span className={`${styles.burger} ${isOpen ? styles.open : ""}`}></span>
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,19 +1,23 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../context/CartContextProvider.jsx";
 
 function CartPage() {
   const { cart, dispatchCart } = useContext(CartContext);
+  const [isCartEmpty, setIsCartEmpty] = useState(true);
 
   useEffect(() => {
+    setIsCartEmpty(cart.length === 0);
     console.log(cart);
   }, [cart]);
 
   return (
-    <>
-      {cart.map((item) => (
-        <div key={item.id}>{item.title} has been added</div>
-      ))}
-    </>
+    <div>
+      {isCartEmpty && <p>Sorry your cart is empty, please add some items</p>}
+      {!isCartEmpty &&
+        cart.map((item) => (
+          <div key={item.id}>{item.title} has been added</div>
+        ))}
+    </div>
   );
 }
 
