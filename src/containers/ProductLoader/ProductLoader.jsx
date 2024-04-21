a874f0d573b4fdb6f3b5c9e8832080ca4cec8e48
staceyjf <staceyfanner@gmail.com>
2024-04-21 14:14:48 +1000

    tweak to card design

--- src/containers/ProductLoader/ProductLoader.jsx
+++ src/containers/ProductLoader/ProductLoader.jsx
@@ -14,36 +14,38 @@ function ProductLoader() {
   const [fetchStatus, setFetchStatus] = useState("");
   const [error, setError] = useState(null);
 
-  
   // get the products from firebase
   useEffect(() => {
     setFetchStatus("LOADING");
     getAllProducts("womens")
-    .then((data) => {
-          setFetchStatus("SUCCESS");
-          setProducts(data);
-        })
-        .catch((e) => {
-          setFetchStatus("FAILED");
-          console.warn(e.message);
-          setError(e);
-        });
+      .then((data) => {
+        setFetchStatus("SUCCESS");
+        setProducts(data);
+      })
+      .catch((e) => {
+        setFetchStatus("FAILED");
+        console.warn(e.message);
+        setError(e);
+      });
   }, []);
-  
+
   // find the product by id
   useEffect(() => {
     const productById = products.find((product) => product.id === id);
 
     if (productById) setProduct(productById);
-   
   }, [id, products]);
-  
+
   return (
     <div>
       {fetchStatus === "LOADING" && <LoadingSpinner />}
-      {fetchStatus === "FAILED" &&  <Message severity="error" message={error.message} />}
-      {!product && fetchStatus === "SUCCESS" && <ProductGrid products={products} category="womens" />}
-      {product && <ProductPage product={product} /> }
+      {fetchStatus === "FAILED" && (
+        <Message severity="error" message={error.message} />
+      )}
+      {!product && fetchStatus === "SUCCESS" && (
+        <ProductGrid products={products} category="womens" />
+      )}
+      {product && <ProductPage product={product} />}
     </div>
   );
 }
